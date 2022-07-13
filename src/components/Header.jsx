b5785dc569e95e4f6eaeb1c5d171641d25d18a9a
OlgaDergacheva <a.ekman@yandex.ru>
2022-07-13 13:57:40 +0300

    react
    react

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Header.css';
+const Header = () => {
+    return <header className='header'>
+        <img src='https://ecoegg.ru/wp-content/uploads/2017/03/love-it-or-return-it-logo-300x300.png' alt='Head' />
+    </header>;
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Profile.css'
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+   <img src='https://img.fonwall.ru/o/2p/canada-moraine-lake-ozero-gory.jpg?route=mid&amp;h=750'alt="profile"/>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+export default Profile;
